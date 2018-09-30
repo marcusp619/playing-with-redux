c08c36a6dbe07452eac051385544677ecd23f642
marcusp619 <marcusp619@gmail.com>
2018-09-29 22:19:26 -0600

    Add container for user list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { createStore } from 'redux';
 import './App.css';
+import { Provider } from 'react-redux';
 import { allReducers } from './reducers/index';
+import UserList from './containers/user-list';
+
 const store = createStore(allReducers); // all data for the whole app
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Playing with Redux</h1>
-        </header>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Playing with Redux</h1>
+          </header>
+          <h2>Username List:</h2>
+          <UserList />
+          <hr /> 
+          <h2>User Details</h2>
+        </div>
+      </Provider>
     );
   }
 }
